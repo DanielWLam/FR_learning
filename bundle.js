c7727c9105b1d5ce24c3f5e5c420b1352f553f70
DanielWLam <lwd931227@126.com>
2017-02-28 23:52:57 +0800

    添加代码，使用了高阶函数（闭包），但没有immutable

--- bundle.js
+++ bundle.js
@@ -63,20 +63,79 @@
 /******/ 	__webpack_require__.p = "";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 3:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var arr = [1, 2, 3, 4, 5];
+// // // ori
+// // // function validateSsn(ssn) {
+// // // 	if (/^\d{3}-\d{2}-\d{4}$/.exec(ssn)) {
+// // // 		console.log('Valid ssn');
+// // // 	} else {
+// // // 		console.log('invalid ssn');
+// // // 	}
+// // // }
 
-console.log(arr.slice(0, 3));
-console.log(arr.slice(0, 3));
+// // // function validatePhone(phone) {
+// // // 	if (/^(\d{3})\d{3}-\d{4}$/.exec(phone)) {
+// // // 		console.log('valid phone');
+// // // 	} else {
+// // // 		console.log('invalid phone');
+// // // 	}
+// // // }
+
+// // function validateValue(value, regx, type) {
+// // 	if (regx.exec(value)) {
+// // 		console.log('valid ' + type);
+// // 	} else {
+// // 		console.log('invalid ' + type);
+// // 	}
+// // }
+
+// // //ori
+// // function validateAddress(address) {
+// // 	if (parseAddress(address)) {
+// // 		console.log(true);
+// // 	} else {
+// // 		console.log(false);
+// // 	}
+// // }
+
+// // function validateName(name) {
+// // 	return parseName(name);
+// // }
+
+// // 本质上就是返回一个闭包
+// function validateValueWithFunc(value, parseFunc, type) {
+// 	return parseFunc(value);
+// }
+
+// // validateValueWithFunc('123-45-6789', /^\d{3}-\d{2}-\d{4}$/.exec, 'SSN');
+
+// function makeRegexParser(regex) {
+// 	return regex.exec;
+// }
+// var parseSsn = makeRegexParser(/^\d{3}-\d{2}-\d{4}$/);
+// var parsePhone = makeRegexParser(/^(\d{3})\d{3}-\d{4}$/);
+// validateValueWithFunc('123-45-6789', parsePhone, 'phone');
+
+var makeAdder = function makeAdder(constantValue) {
+  return function (value) {
+    return constantValue + value;
+  };
+};
+
+var add10 = makeAdder(10);
+console.log(add10(20));
+console.log(add10(30));
 
 /***/ })
-/******/ ]);
+
+/******/ });
--- index.js
+++ index.js
@@ -1,9 +1,57 @@
-import _ from 'lodash';
-var sin = _.memorize(x => Math.sin(x));
-var start = + new Date();
-var a = sin(1);
-console.log(a);
-conosle.log(+new Date() - start);
-var b = sin(1);
-console.log(+new Date() - start);
-console.log(b);
+// // // ori
+// // // function validateSsn(ssn) {
+// // // 	if (/^\d{3}-\d{2}-\d{4}$/.exec(ssn)) {
+// // // 		console.log('Valid ssn');
+// // // 	} else {
+// // // 		console.log('invalid ssn');
+// // // 	}
+// // // }
+
+// // // function validatePhone(phone) {
+// // // 	if (/^(\d{3})\d{3}-\d{4}$/.exec(phone)) {
+// // // 		console.log('valid phone');
+// // // 	} else {
+// // // 		console.log('invalid phone');
+// // // 	}
+// // // }
+
+// // function validateValue(value, regx, type) {
+// // 	if (regx.exec(value)) {
+// // 		console.log('valid ' + type);
+// // 	} else {
+// // 		console.log('invalid ' + type);
+// // 	}
+// // }
+
+// // //ori
+// // function validateAddress(address) {
+// // 	if (parseAddress(address)) {
+// // 		console.log(true);
+// // 	} else {
+// // 		console.log(false);
+// // 	}
+// // }
+
+// // function validateName(name) {
+// // 	return parseName(name);
+// // }
+
+// // 本质上就是返回一个闭包
+// function validateValueWithFunc(value, parseFunc, type) {
+// 	return parseFunc(value);
+// }
+
+// // validateValueWithFunc('123-45-6789', /^\d{3}-\d{2}-\d{4}$/.exec, 'SSN');
+
+// function makeRegexParser(regex) {
+// 	return regex.exec;
+// }
+// var parseSsn = makeRegexParser(/^\d{3}-\d{2}-\d{4}$/);
+// var parsePhone = makeRegexParser(/^(\d{3})\d{3}-\d{4}$/);
+// validateValueWithFunc('123-45-6789', parsePhone, 'phone');
+
+let makeAdder = constantValue => value => constantValue + value;
+
+var add10 = makeAdder(10);
+console.log(add10(20));
+console.log(add10(30));
